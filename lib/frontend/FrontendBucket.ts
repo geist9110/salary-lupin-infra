f00d2ba9ece293ccfb368044bd39dc16d3f3ee3a
geist9110 <geist9110@naver.com>
2025-04-30 22:11:37 +0900

    refactor: frontend에서 사용하는 bucket을 별도의 파일로 분리

---
+++ lib/frontend/FrontendBucket.ts
@@ -0,0 +1,26 @@
+import { Construct } from "constructs";
+import * as s3 from "aws-cdk-lib/aws-s3";
+import { RemovalPolicy } from "aws-cdk-lib";
+
+export class FrontendBucket extends Construct {
+  public readonly frontendBucket: s3.Bucket;
+  public readonly pipelineSourceBucket: s3.Bucket;
+
+  constructor(scope: Construct, id: string, env: string) {
+    super(scope, id);
+
+    this.frontendBucket = new s3.Bucket(this, "FrontendBucket", {
+      bucketName: process.env.FRONTEND_BUCKET_NAME,
+      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
+      removalPolicy: RemovalPolicy.DESTROY,
+      autoDeleteObjects: true,
+    });
+
+    this.pipelineSourceBucket = new s3.Bucket(this, "PipelineSourceBucket", {
+      bucketName: `frontend-codepipeline-source-${env}`,
+      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
+      removalPolicy: RemovalPolicy.DESTROY,
+      autoDeleteObjects: true,
+    });
+  }
+}
--- lib/frontend/frontend-stack.ts
+++ lib/frontend/frontend-stack.ts
@@ -1,7 +1,7 @@
-import { RemovalPolicy, Stack, StackProps } from "aws-cdk-lib";
+import { Stack, StackProps } from "aws-cdk-lib";
 import { Construct } from "constructs";
-import * as s3 from "aws-cdk-lib/aws-s3";
 import { FrontendCodeBuild } from "./FrontendCodeBuild";
+import { FrontendBucket } from "./FrontendBucket";
 
 export class FrontendStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
@@ -9,20 +9,7 @@ export class FrontendStack extends Stack {
 
     const env = process.env.NODE_ENV ?? "dev";
 
-    const frontendBucket = new s3.Bucket(this, "FrontendBucket", {
-      bucketName: process.env.FRONTEND_BUCKET_NAME,
-      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
-      removalPolicy: RemovalPolicy.DESTROY,
-      autoDeleteObjects: true,
-    });
-
-    const PipelineSourceBucket = new s3.Bucket(this, "PipelineSourceBucket", {
-      bucketName: `frontend-codepipeline-source-${env}`,
-      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
-      removalPolicy: RemovalPolicy.DESTROY,
-      autoDeleteObjects: true,
-    });
-
+    const buckets = new FrontendBucket(this, "Bucket", env);
     const codeBuild = new FrontendCodeBuild(this, "CodeBuild", env);
   }
 }
