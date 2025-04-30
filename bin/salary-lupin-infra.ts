079bdb707f705a06102a8cc811f0ce27792848f0
geist9110 <geist9110@naver.com>
2025-04-30 21:10:10 +0900

    feat: Env 파일을 통한 변수 관리 추가

--- bin/salary-lupin-infra.ts
+++ bin/salary-lupin-infra.ts
@@ -1,10 +1,14 @@
 #!/usr/bin/env node
 import * as cdk from "aws-cdk-lib";
 import { FrontendStack } from "../lib/frontend/frontend-stack";
+import * as dotenv from "dotenv";
+
+const env = process.env.NODE_ENV ?? "dev";
+dotenv.config({ path: `env/${env}.env` });
 
 const app = new cdk.App();
 
-new FrontendStack(app, "FrontendStack", {
+new FrontendStack(app, `FrontendStack-${env}`, {
   env: {
     region: "ap-northeast-2",
   },
--- lib/frontend/frontend-stack.ts
+++ lib/frontend/frontend-stack.ts
@@ -1,4 +1,4 @@
-import { RemovalPolicy, Stack, StackProps } from "aws-cdk-lib";
+import { Stack, StackProps } from "aws-cdk-lib";
 import { Construct } from "constructs";
 import * as s3 from "aws-cdk-lib/aws-s3";
 
@@ -6,10 +6,8 @@ export class FrontendStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    new s3.Bucket(this, "SalaryLupinFrontendBucket", {
-      versioned: true,
-      removalPolicy: RemovalPolicy.DESTROY,
-      autoDeleteObjects: true,
+    new s3.Bucket(this, "FrontendBucket", {
+      bucketName: process.env.FRONTEND_BUCKET_NAME,
     });
   }
 }
