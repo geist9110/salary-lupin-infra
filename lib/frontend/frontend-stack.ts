7630f40baf6c1b33e34c1a84225fc2af8d7e5a09
geist9110 <geist9110@naver.com>
2025-04-30 21:30:22 +0900

    feat: codePipeline의 Source 버켓 추가

--- lib/frontend/frontend-stack.ts
+++ lib/frontend/frontend-stack.ts
@@ -1,4 +1,4 @@
-import { Stack, StackProps } from "aws-cdk-lib";
+import { RemovalPolicy, Stack, StackProps } from "aws-cdk-lib";
 import { Construct } from "constructs";
 import * as s3 from "aws-cdk-lib/aws-s3";
 
@@ -6,8 +6,20 @@ export class FrontendStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
+    const env = process.env.NODE_ENV ?? "dev";
+
     new s3.Bucket(this, "FrontendBucket", {
       bucketName: process.env.FRONTEND_BUCKET_NAME,
+      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
+      removalPolicy: RemovalPolicy.DESTROY,
+      autoDeleteObjects: true,
+    });
+
+    new s3.Bucket(this, "FrontendCodePipelineSourceBucket", {
+      bucketName: `frontend-codepipeline-source-${env}`,
+      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
+      removalPolicy: RemovalPolicy.DESTROY,
+      autoDeleteObjects: true,
     });
   }
 }
