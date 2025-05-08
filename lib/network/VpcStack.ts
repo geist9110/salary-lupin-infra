5a948cb5d68748d5b61fba51b6b48461d0b114e9
geist9110 <geist9110@naver.com>
2025-05-08 16:59:53 +0900

    feat: VPC 스택 추가

---
+++ lib/network/VpcStack.ts
@@ -0,0 +1,30 @@
+import { Stack } from "aws-cdk-lib";
+import { Construct } from "constructs";
+import { SubnetType, Vpc } from "aws-cdk-lib/aws-ec2";
+
+interface VpcStackProps {
+  environment: string;
+}
+
+export class VpcStack extends Stack {
+  public readonly vpc: Vpc;
+
+  constructor(scope: Construct, id: string, props: VpcStackProps) {
+    super(scope, id);
+
+    const vpc = new Vpc(this, `Salary-lupin-VPC-${props.environment}`, {
+      maxAzs: 2,
+      natGateways: 0,
+      subnetConfiguration: [
+        {
+          name: "public-subnet",
+          subnetType: SubnetType.PUBLIC,
+        },
+        {
+          name: "private-subnet",
+          subnetType: SubnetType.PRIVATE_WITH_EGRESS,
+        },
+      ],
+    });
+  }
+}
