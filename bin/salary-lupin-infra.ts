7bc7f2d315bcd76f8271c193d6925d1a68e66d90
geist9110 <geist9110@naver.com>
2025-05-08 17:00:33 +0900

    feat: VPC 스택을 배포파일에 통합

--- bin/salary-lupin-infra.ts
+++ bin/salary-lupin-infra.ts
@@ -3,32 +3,25 @@ import * as cdk from "aws-cdk-lib";
 import { FrontendStack } from "../lib/frontend/frontend-stack";
 import * as dotenv from "dotenv";
 import { DomainStack } from "../lib/domain/DomainStack";
+import { VpcStack } from "../lib/network/VpcStack";
 
 const environment = process.env.NODE_ENV ?? "dev";
 dotenv.config({ path: `env/${environment}.env` });
 
 const app = new cdk.App();
 
-const domainName = process.env.DOMAIN_NAME;
-const accountId = process.env.ACCOUNT_ID;
+const domainName = process.env.DOMAIN_NAME!;
+const accountId = process.env.ACCOUNT_ID!;
 
-const appName = process.env.APP_NAME;
-const githubOwner = process.env.GITHUB_OWNER;
-const githubRepo = process.env.GITHUB_REPO_FRONTEND;
-const githubConnectionArn = process.env.GITHUB_CONNECTION_ARN;
-const githubBranch = process.env.BRANCH;
+const appName = process.env.APP_NAME!;
+const githubOwner = process.env.GITHUB_OWNER!;
+const githubRepo = process.env.GITHUB_REPO_FRONTEND!;
+const githubConnectionArn = process.env.GITHUB_CONNECTION_ARN!;
+const githubBranch = process.env.BRANCH!;
 
-if (
-  !domainName ||
-  !accountId ||
-  !appName ||
-  !githubOwner ||
-  !githubRepo ||
-  !githubConnectionArn ||
-  !githubBranch
-) {
-  throw new Error(`Check ${environment}.env file`);
-}
+const vpcStack = new VpcStack(app, `VpcStack-${environment}`, {
+  environment: environment,
+});
 
 const domainStack = new DomainStack(app, "DomainStack", {
   environment: environment,
