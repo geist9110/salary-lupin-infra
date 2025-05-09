774c68239a62f5ac6ab0f047cc88ce60c4775dad
geist9110 <geist9110@naver.com>
2025-05-09 13:19:43 +0900

    feat: ASG 추가

--- lib/backend/BackendStack.ts
+++ lib/backend/BackendStack.ts
@@ -1,7 +1,14 @@
 import { Stack } from "aws-cdk-lib";
 import { Construct } from "constructs";
-import { Cluster } from "aws-cdk-lib/aws-ecs";
-import { Vpc } from "aws-cdk-lib/aws-ec2";
+import { Cluster, EcsOptimizedImage } from "aws-cdk-lib/aws-ecs";
+import {
+  InstanceClass,
+  InstanceSize,
+  InstanceType,
+  SubnetType,
+  Vpc,
+} from "aws-cdk-lib/aws-ec2";
+import { AutoScalingGroup } from "aws-cdk-lib/aws-autoscaling";
 
 interface BackendStackProps {
   environment: string;
@@ -12,8 +19,22 @@ export class BackendStack extends Stack {
   constructor(scope: Construct, id: string, props: BackendStackProps) {
     super(scope, id);
 
-    const ecs = new Cluster(this, `BackendCluster-${props.environment}`, {
+    const ecs = new Cluster(this, `Backend-Cluster-${props.environment}`, {
       vpc: props.vpc,
     });
+
+    const autoScalingGroup = new AutoScalingGroup(
+      this,
+      `Backend-AutoScaling-Group-${props.environment}`,
+      {
+        vpc: props.vpc,
+        vpcSubnets: props.vpc.selectSubnets({
+          subnetType: SubnetType.PRIVATE_WITH_EGRESS,
+        }),
+        instanceType: InstanceType.of(InstanceClass.T3, InstanceSize.MICRO),
+        machineImage: EcsOptimizedImage.amazonLinux2(),
+        desiredCapacity: 1,
+      },
+    );
   }
 }
