2de3cec8f1f5aa2ace7f01d5e046e1d2f400ea45
geist9110 <geist9110@naver.com>
2025-05-10 13:01:09 +0900

    fix: LB의 target group의 순환 참조 문제 수정

--- lib/backend/BackendLoadBalancer.ts
+++ lib/backend/BackendLoadBalancer.ts
@@ -1,6 +1,11 @@
 import { Duration, Stack } from "aws-cdk-lib";
 import { Construct } from "constructs";
-import { ApplicationLoadBalancer } from "aws-cdk-lib/aws-elasticloadbalancingv2";
+import {
+  ApplicationLoadBalancer,
+  ApplicationProtocol,
+  ApplicationTargetGroup,
+  TargetType,
+} from "aws-cdk-lib/aws-elasticloadbalancingv2";
 import { SecurityGroup, SubnetType, Vpc } from "aws-cdk-lib/aws-ec2";
 import { Ec2Service } from "aws-cdk-lib/aws-ecs";
 
@@ -33,16 +38,23 @@ export class BackendLoadBalancer extends Stack {
       { port: 80 },
     );
 
-    listener.addTargets(
-      `Backend-ApplicationLoadBalancer-Target-${props.environment}`,
+    const targetGroup = new ApplicationTargetGroup(
+      this,
+      `Backend-ApplicationLoadBalancer-TargetGroups-${props.environment}`,
       {
+        vpc: props.vpc,
         port: 80,
-        targets: [props.target],
+        protocol: ApplicationProtocol.HTTP,
+        targetType: TargetType.INSTANCE,
         healthCheck: {
           path: "/",
           interval: Duration.seconds(30),
         },
       },
     );
+
+    listener.addTargetGroups(`Add-BackendTargetGroup-${props.environment}`, {
+      targetGroups: [targetGroup],
+    });
   }
 }
