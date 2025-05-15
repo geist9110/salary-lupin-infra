6c31bb47cd6f4e88ad2ca1c331c8d2a705e8c079
geist9110 <geist9110@naver.com>
2025-05-15 12:38:15 +0900

    refactor: FrontendBucket을 webBucket과 artifactBucket으로 변경

--- lib/frontend/frontend-stack.ts
+++ lib/frontend/frontend-stack.ts
@@ -1,10 +1,10 @@
 import { Stack, StackProps } from "aws-cdk-lib";
 import { Construct } from "constructs";
 import { FrontendCodeBuild } from "./FrontendCodeBuild";
-import { FrontendBucket } from "./FrontendBucket";
 import { FrontendCodePipeline } from "./FrontendCodePipeline";
-import { applyCommonTags } from "../util/applyCommonTags";
 import { FrontendCloudfront } from "./FrontendCloudfront";
+import { WebBucket } from "../storage/WebBucket";
+import { ArtifactBucket } from "../storage/ArtifactBucket";
 
 interface FrontendStackProps extends StackProps {
   appName: string;
@@ -22,14 +22,17 @@ export class FrontendStack extends Stack {
   constructor(scope: Construct, id: string, props: FrontendStackProps) {
     super(scope, id, props);
 
-    const buckets = new FrontendBucket(this, "Bucket", {
-      appName: props.appName,
+    const webBucket = new WebBucket(this, {
       environment: props.environment,
-    });
+    }).bucket;
+
+    const artifactBucket = new ArtifactBucket(this, {
+      environment: props.environment,
+    }).bucket;
 
     const codeBuild = new FrontendCodeBuild(this, "CodeBuild", {
       environment: props.environment,
-      artifactBucket: buckets.pipelineArtifactBucket,
+      artifactBucket: artifactBucket,
     });
 
     const codePipeline = new FrontendCodePipeline(this, "Pipeline", {
@@ -39,29 +42,16 @@ export class FrontendStack extends Stack {
       githubBranch: props.githubBranch,
       githubConnectionArn: props.githubConnectionArn,
       buildProject: codeBuild.build,
-      targetBucket: buckets.frontendBucket,
-      artifactBucket: buckets.pipelineArtifactBucket,
+      targetBucket: webBucket,
+      artifactBucket: artifactBucket,
     });
 
     const cloudfront = new FrontendCloudfront(this, "Cloudfront", {
       environment: props.environment,
-      bucket: buckets.frontendBucket,
+      bucket: webBucket,
       certificateArn: props.certificateArn,
       hostedZoneId: props.hostedZoneId,
       hostedZoneName: props.hostedZoneName,
-    });
-
-    applyCommonTags({
-      resources: [
-        buckets.frontendBucket,
-        buckets.pipelineArtifactBucket,
-        codeBuild.role,
-        codeBuild.build,
-        codePipeline.pipeline,
-        cloudfront.distribution,
-      ],
-      appName: props.appName,
-      environment: props.environment,
     });
   }
 }
